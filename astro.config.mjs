0a95ef47b399e003f6685a47aa1091e93dde68ab
adam weng <wengjin456123@gmail.com>
2024-08-04 20:27:55 +0800

    fix about me

--- astro.config.mjs
+++ astro.config.mjs
@@ -31,7 +31,7 @@ const oklchToHex = (str) => {
 // https://astro.build/config
 export default defineConfig({
   site: "https://wengjincheng.github.io",
-  base: "/myBlog",
+  base: "/",
   trailingSlash: "always",
   integrations: [
     tailwind(),
